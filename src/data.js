d1f6ce27b067f664c549778090308bd13b5aa85b
kelly <kellyp.alves87@gmail.com>
2020-05-08 12:03:37 -0300

    ordenação dos cards nos menus, paginação dos cards, alteração/inclusão de divs para paginação

--- src/data.js
+++ src/data.js
@@ -1,36 +1,55 @@
-export function filterData (data, key, value) {
-  function filter (item) {
-    return item[key] === value
+export function filterData(data, key, value) {
+  function filter(item) {
+    return item[key] === value;
   }
-  return data.filter(filter)
-};
+  return data.filter(filter);
+}
+
+export function filterDataOptions(data, key, attr, value) {
+  function filter(item) {
+    return item[key][attr] === value;
+  }
+  return data.filter(filter);
+}
 
 export function sortData(data, name) {
-    if (name === "A-Z") {
-    return data.sortAsc((a, b) => (a.name > b.name ? 1 : -1));
+  if (name === "a-z") {
+    return data.sort((a, b) => (a.name > b.name ? 1 : -1));
   }
-    return data.sortDesc((a, b) => (a, name > b.name ? -1 : 1));
-};
+  return data.sort((a, b) => (a, name > b.name ? -1 : 1));
+}
 
 export function statusCharacters(data, name, value) {
-    return data.filterData(data, name, value);
-};
+  return data.filterData(data, name, value);
+}
 
-export function genderCharacters (data, name, value) {
-    return data.filterData(data, name, value);
-};
+export function genderCharacters(data, name, value) {
+  return data.filterData(data, name, value);
+}
 
-export function specieCharacters (data, name, value) {
-    return data.filterData(data, name, value);
-};
+export function specieCharacters(data, name, value) {
+  return data.filterData(data, name, value);
+}
 
-export function originCharacters (data, name, value) {
-    return data.filterData(data, name, value);
-};
+export function originCharacters(data, name, value) {
+  return data.filterData(data, name, value);
+}
 
-export function locationCharacters (data, name, value) {
-    return data.filterData(data, name, value);
-};
+export function locationCharacters(data, name, value) {
+  return data.filterData(data, name, value);
+}
 
+export function removeDuplicates(data, name) {
+  return data.reduce((unique, elem) => {
+    if (!unique.some((ob) => ob[name].name === elem[name].name)) {
+      unique.push(elem);
+    }
+    return unique;
+  }, []);
+}
 
-  
+export function searchName(data, name) {
+  return data.filter((item) =>
+    item.name.toUpperCase().includes(name.toUpperCase())
+  );
+}
