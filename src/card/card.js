a1e9da634d307816fa655ef9a0ade3c1e5217680
Aline <alinesanqueza@gmail.com>
2020-05-13 12:37:03 -0300

    barra de navegação ajustada

--- src/card/card.js
+++ src/card/card.js
@@ -3,14 +3,14 @@
 export function generateCard(item, index) { //função criada para geração dos Cards que recebe um item e o index(dados do html).
     return `<div id="${index}"> 
         <img src="${item.image}">
-        <h3>Nome: ${item.name}</h3>
-        <h3>Status: ${item.status}</h3>
-        <h3>Specie: ${item.species}</h3>
-        <h3>Gênero: ${item.gender}</h3>
-        <h3>Origem: ${item.origin.name}</h3>
-        <h3>Localização:${item.location.name}</h3>
+        <p class="pnome">${item.name}</p>
+        <p class="parag">Status: ${item.status}</p>
+        <p class="parag">Specie: ${item.species}</p>
+        <p class="parag">Gênero: ${item.gender}</p>
+        <p class="parag">Origem: ${item.origin.name}</p>
+        <p class="parag">Localização: ${item.location.name}</p>
         </div>
-      `;
-  }
+        `;
+    }
 
   //que nos retorna uma div com todos os itens que desejamos que apareça no card.
