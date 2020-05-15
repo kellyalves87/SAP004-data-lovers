234c65825beeb38e456fce81205e59a86eef371e
kelly <kellyp.alves87@gmail.com>
2020-05-15 17:05:41 -0300

    Updates

--- data.js
+++ data.js
@@ -2,22 +2,27 @@ export function filterData(data, key, value) {
   function filter(item) {
     return item[key] === value;
   }
-    return data.filter(filter);
+  return data.filter(filter);
 }
 
 export function filterDataOptions(data, key, attr, value) {
   function filter(item) {
     return item[key][attr] === value;
   }
-    return data.filter(filter);
+  return data.filter(filter);
 }
 
 export function sortData(data, name, optionValue) {
   if (optionValue === "A-Z") {
     return data.sort((a, b) => (a[name] > b[name] ? 1 : -1));
   }
-    return data.sort((a, b) => (a[name] > b[name] ? -1 : 1)); 
+  return data.sort((a, b) => (a[name] > b[name] ? -1 : 1));
+}
 
+export function sortDataNested(data, name, value) {
+  return data.sort((elem1, elem2) =>
+    elem1[name][value] > elem2[name][value] ? 1 : -1
+  );
 }
 
 export function removeDuplicates(data, name) {
@@ -30,7 +35,22 @@ export function removeDuplicates(data, name) {
 }
 
 export function searchName(data, name, value) {
-    return data.filter((item) =>
+  return data.filter((item) =>
     item[name].toUpperCase().includes(value.toUpperCase())
   );
 }
+
+export function makeStatistics(data, name, value) {
+  if (!data || !name) {
+    return TypeError('no data has been provided');
+  }
+
+  let typeName = 0
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][name].includes(value)) {
+      typeName++;
+    }
+  }
+  let finalPercentage = typeName / data.length * 100
+  return finalPercentage.toFixed(2)
+};
--- main.js
+++ main.js
@@ -1,72 +1,79 @@
 import data from "./data/rickandmorty/rickandmorty.js";
 import { dropDown } from "./dropdown/dropdown.js";
-import { removeDuplicates } from "./data.js";
+import { removeDuplicates, sortDataNested, makeStatistics } from "./data.js";
 import {
   onChange,
   onChangeSearchByNestedObject,
   onChangeSortData,
   onChangeSearchName,
 } from "./event/event.js";
-import { nextPage, prevPage } from "./pagination/pagination.js";
+import {nextPage, prevPage} from "./pagination/pagination.js";
 
-const duplicatesOrigin = removeDuplicates(data.results, "origin");
+const duplicatesOrigin = removeDuplicates(data.results, "origin"); 
 const dropDownOrigin = document.getElementById("origin");
-dropDownOrigin.innerHTML = dropDown(duplicatesOrigin, "origin", "Origem");
-
-const duplicatesLocation = removeDuplicates(data.results, "location");
+dropDownOrigin.innerHTML = dropDown(sortDataNested(duplicatesOrigin, "origin", "name"), "origin", "Origem");
+const duplicatesLocation = removeDuplicates(data.results, "location"); 
 const dropDownLocalation = document.getElementById("location");
-dropDownLocalation.innerHTML = dropDown(
-  duplicatesLocation,
-  "location",
-  "Localização"
-);
+dropDownLocalation.innerHTML = dropDown(sortDataNested(duplicatesLocation, "location", "name"), "location", "Localização");
 
 let abstract = document.getElementById("abstract");
 
 const status = document.getElementById("status");
-status.addEventListener("change", function () {
-  abstract.style.display = "none";
+status.addEventListener("change", function () { 
+  abstract.style.display = "none"; 
   onChange(data.results, "status", status.value);
+  const percentage = makeStatistics(data.results, "status", status.value);
+
+  let result = document.getElementById("statistics");
+  result.innerHTML = `${percentage}% dos personagens estão no status: ${status.value}`;
 });
 
-const gender = document.getElementById("gender");
-gender.addEventListener("change", function () {
+const gender = document.getElementById("gender"); 
+gender.addEventListener("change", function () { 
   onChange(data.results, "gender", gender.value);
+  const percentageGender = makeStatistics(data.results, "gender", gender.value);
+
+  let result = document.getElementById("statistics");
+  result.innerHTML = `${percentageGender}% dos personagens estão no gênero: ${gender.value}`;
 });
 
-const species = document.getElementById("species");
-species.addEventListener("change", function () {
+const species = document.getElementById("species"); 
+species.addEventListener("change", function () { 
   onChange(data.results, "species", species.value);
+  const percentageSpecies = makeStatistics(data.results, "species", species.value);
+
+  let result = document.getElementById("statistics");
+  result.innerHTML = `${percentageSpecies}% dos personagens estão nas espécies: ${species.value}`;
+   
 });
 
-const origin = document.getElementById("origin");
+const origin = document.getElementById("origin"); 
 origin.addEventListener("change", function () {
-  onChangeSearchByNestedObject(data.results, "origin", "name", origin.value);
+  onChangeSearchByNestedObject(data.results, "origin", "name", origin.value); 
 });
 
 const location = document.getElementById("location");
 location.addEventListener("change", function () {
-  onChangeSearchByNestedObject(
-    data.results,
+  onChangeSearchByNestedObject( 
     "location",
     "name",
     location.value
-  );
+  ); 
 });
 
-const sort = document.getElementById("sort");
-sort.addEventListener("change", function () {
-  const selectedIndex = sort.selectedIndex;
+const sort = document.getElementById("sort"); 
+sort.addEventListener("change", function () { 
+  const selectedIndex = sort.selectedIndex; 
   onChangeSortData(data.results, "name", sort[selectedIndex].value);
 });
 
-const searchText = document.getElementById("search-name");
+const searchText = document.getElementById("search-name"); 
 searchText.addEventListener("keypress", function () {
   onChangeSearchName(data.results, "name", searchText.value);
 });
 
-const nextPagination = document.getElementById("btn-next");
-nextPagination.addEventListener("click", function () {
+const nextPagination = document.getElementById("btn-next"); 
+nextPagination.addEventListener("click", function () { 
   nextPage();
 });
 
@@ -74,3 +81,4 @@ const prevPagination = document.getElementById("btn-prev");
 prevPagination.addEventListener("click", function () {
   prevPage();
 });
+
