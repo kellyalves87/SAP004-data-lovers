c3980687b9d58f5c9a54344f6f1966023ca92168
Aline <alinesanqueza@gmail.com>
2020-05-16 12:57:25 -0300

    tentativa de um menu hamburguer

--- src/card/card.js
+++ src/card/card.js
@@ -1,12 +1,12 @@
 export function generateCard(item, index) { 
     return `<li id="${index}"> 
         <img src="${item.image}">
-        <p class="paragrafo-nome">${item.name}</p>
-        <p class="paragrafo">Status: ${item.status}</p>
-        <p class="paragrafo">Specie: ${item.species}</p>
-        <p class="paragrafo">Gênero: ${item.gender}</p>
-        <p class="paragrafo">Origem: ${item.origin.name}</p>
-        <p class="paragrafo">Localização: ${item.location.name}</p>
+        <p class="paragraph-name">${item.name}</p>
+        <p class="paragraph">Status: ${item.status}</p>
+        <p class="paragraph">Specie: ${item.species}</p>
+        <p class="paragraph">Gênero: ${item.gender}</p>
+        <p class="paragraph">Origem: ${item.origin.name}</p>
+        <p class="paragraph">Localização: ${item.location.name}</p>
         </li>
         `;
     }
